3771dec445a3605458232b52a43d446bf1aed185
Shashank Kumar <39635214+sneakatyou@users.noreply.github.com>
2023-10-15 18:31:04 +0530

    added readme

--- models/nca/substeps/evolve_cell/action.py
+++ models/nca/substeps/evolve_cell/action.py
@@ -9,7 +9,6 @@ class GenerateAliveMask(SubstepAction):
         observation_grid = observation['AliveState']
         threshold = torch.tensor(self.config['simulation_metadata']['alive_threshold_value'])
         alive_mask = torch.gt(observation_grid, threshold)
-        # alive_mask = observation_grid > 0.1 #convert 0.1 to config variable
         
         return {self.output_variables[0] : alive_mask}
 

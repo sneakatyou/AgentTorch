6c8139ccf9bada8369b71809e80f90a369c4316b
Shashank Kumar <39635214+sneakatyou@users.noreply.github.com>
2023-12-03 13:54:00 +0530

    df

--- models/nca/trainNca.py
+++ models/nca/trainNca.py
@@ -51,87 +51,98 @@ class TrainNca:
             f"Target is: {self.runner.config['simulation_metadata']['target']}",)
 
         for i in range(self.runner.config['simulation_metadata']['num_episodes']):
-            step_n = np.random.randint(64, 96)
-            #step_n = 5
-            overflow_loss = 0.0
-            diff_loss = 0.0
-            target_loss = 0.0
-            aux_target_loss = 0.0
-            self.runner.reset()
-            self.opt.zero_grad()
-
-            self.runner.step(step_n)  # its is sampled randomly right now
-            outputs = self.runner.state_trajectory[-1][-step_n:]
-            
-            list_outputs = [outputs[i]['agents']['automata']
-                            ['cell_state'] for i in range(step_n)]
-            x_intermediate_steps = torch.stack(list_outputs, dim=0)
-            x_intermediate_steps = x_intermediate_steps.permute(
-                [1, 0, 4, 2, 3])
-            overflow_loss = (x_intermediate_steps-x_intermediate_steps.clamp(-2.0, 2.0)
-                                )[:,:,:self.SCALAR_CHN].square().sum()
-
-            final_step_output = outputs[-1]
-            x_final_step = final_step_output['agents']['automata']['cell_state']
-            x_final_step = x_final_step.permute([0, 3, 1, 2])
-            target_loss = self.target_loss_f(
-                x_final_step[:, :self.target.shape[0]])
-
-            target_loss /= 2.
-            aux_target_loss /= 2.
-            diff_loss = diff_loss*10.0
-            loss = target_loss+overflow_loss+diff_loss + aux_target_loss
-
-            with torch.no_grad():
-                # loss.backward()
-                try:
-                    loss.backward()
-                except Exception as e:
-                    print(e)
-                self.opt.step()
-                self.lr_sched.step()
-                self.loss_log.append(loss.item())
-                
-                if i % 10 == 0:
-                    clear_output(True)
-                    pl.plot(self.loss_log, '.', alpha=0.1)
-                    pl.yscale('log')
-                    pl.ylim(np.min(self.loss_log), self.loss_log[0])
-                    pl.show()
-                    imgs = self.ops.to_rgb(x_final_step)
-                    if self.hex_grid:
-                        imgs = F.grid_sample(imgs, self.xy_grid[None, :].repeat(
-                            [len(imgs), 1, 1, 1]), mode='bicubic')
-                    imgs = imgs.permute([0, 2, 3, 1]).cpu()
-                    self.ops.imshow(self.ops.zoom(
-                        self.ops.tile2d(imgs, 4), 2))  # zoom
-                    if self.AUX_L_TYPE != "noaux":
-                        alphas = x_final_step[:, 3].cpu()
-                        for extra_i in range(self.aux_target.shape[-3]):
-                            imgs = 1. - alphas + alphas * \
-                                (x_final_step[:, 4+extra_i].cpu() + 0.5)
-                        if self.hex_grid:
-                            imgs = F.grid_sample(
-                                imgs[:, None], self.xy_grid[None, :].repeat(
-                                    [len(imgs), 1, 1, 1]).cpu(),
-                                mode='bicubic')[:, 0]
-                        self.ops.imshow(self.ops.zoom(
-                            self.ops.tile2d(imgs, 8), 1))
-
-                if i % 10 == 0:
-                    print('\rstep_n:', len(self.loss_log),
-                            ' loss:', loss.item(),
-                            ' lr:', self.lr_sched.get_lr()[0], end='')
-                
-                if len(self.loss_log) % 500 == 0:
-                    model_name = self.model_suffix + \
-                        "_{:07d}.pt".format(len(self.loss_log))
-                    print(model_name)
-                    torch.save(self.ca.state_dict(), model_name)
+            self.train_step(i)
         
         print("Training complete")
         torch.save(self.runner.state_dict(
         ), self.runner.config['simulation_metadata']['learning_params']['model_path'])
+
+    def train_step(self, i):
+        step_n = np.random.randint(64, 96)
+            # step_n = 5
+        overflow_loss = 0.0
+        diff_loss = 0.0
+        target_loss = 0.0
+        aux_target_loss = 0.0
+        self.runner.reset()
+        self.opt.zero_grad()
+
+        self.runner.step(step_n)  # its is sampled randomly right now
+        x_final_step, loss = self.calculate_loss(
+                step_n, diff_loss,overflow_loss,target_loss, aux_target_loss)
+
+        with torch.no_grad():
+                # loss.backward()
+            try:
+                loss.backward()
+            except Exception as e:
+                print(e)
+            self.opt.step()
+            self.lr_sched.step()
+            self.loss_log.append(loss.item())
+
+            self.display_output(i, x_final_step, loss)
+
+    def calculate_loss(self, step_n, diff_loss,overflow_loss,target_loss, aux_target_loss):
+        outputs = self.runner.state_trajectory[-1][-step_n:]
+            
+        list_outputs = [outputs[i]['agents']['automata']
+                            ['cell_state'] for i in range(step_n)]
+        x_intermediate_steps = torch.stack(list_outputs, dim=0)
+        x_intermediate_steps = x_intermediate_steps.permute(
+                [1, 0, 4, 2, 3])
+        overflow_loss = (x_intermediate_steps-x_intermediate_steps.clamp(-2.0, 2.0)
+                                )[:,:,:self.SCALAR_CHN].square().sum()
+
+        final_step_output = outputs[-1]
+        x_final_step = final_step_output['agents']['automata']['cell_state']
+        x_final_step = x_final_step.permute([0, 3, 1, 2])
+        target_loss = self.target_loss_f(
+                x_final_step[:, :self.target.shape[0]])
+
+        target_loss /= 2.
+        aux_target_loss /= 2.
+        diff_loss = diff_loss*10.0
+        loss = target_loss+overflow_loss+diff_loss + aux_target_loss
+        return x_final_step,loss
+
+    def display_output(self, i, x_final_step, loss):
+        if i % 10 == 0:
+            clear_output(True)
+            pl.plot(self.loss_log, '.', alpha=0.1)
+            pl.yscale('log')
+            pl.ylim(np.min(self.loss_log), self.loss_log[0])
+            pl.show()
+            imgs = self.ops.to_rgb(x_final_step)
+            if self.hex_grid:
+                imgs = F.grid_sample(imgs, self.xy_grid[None, :].repeat(
+                            [len(imgs), 1, 1, 1]), mode='bicubic')
+            imgs = imgs.permute([0, 2, 3, 1]).cpu()
+            self.ops.imshow(self.ops.zoom(
+                        self.ops.tile2d(imgs, 4), 2))  # zoom
+            if self.AUX_L_TYPE != "noaux":
+                alphas = x_final_step[:, 3].cpu()
+                for extra_i in range(self.aux_target.shape[-3]):
+                    imgs = 1. - alphas + alphas * \
+                                (x_final_step[:, 4+extra_i].cpu() + 0.5)
+                if self.hex_grid:
+                    imgs = F.grid_sample(
+                                imgs[:, None], self.xy_grid[None, :].repeat(
+                                    [len(imgs), 1, 1, 1]).cpu(),
+                                mode='bicubic')[:, 0]
+                self.ops.imshow(self.ops.zoom(
+                            self.ops.tile2d(imgs, 8), 1))
+
+        if i % 10 == 0:
+            print('\rstep_n:', len(self.loss_log),
+                            ' loss:', loss.item(),
+                            ' lr:', self.lr_sched.get_lr()[0], end='')
+                
+        if len(self.loss_log) % 500 == 0:
+            model_name = self.model_suffix + \
+                        "_{:07d}.pt".format(len(self.loss_log))
+            print(model_name)
+            torch.save(self.ca.state_dict(), model_name)
 
 
 if __name__ == "__main__":
